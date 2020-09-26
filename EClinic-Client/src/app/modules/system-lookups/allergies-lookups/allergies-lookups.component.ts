9d58dda998f65d7c159307e1b52a2ebc10db8685
sinanshishani <49819673+sinanshishani@users.noreply.github.com>
2020-09-26 20:38:08 +0300

    updates

--- EClinic-Client/src/app/modules/system-lookups/allergies-lookups/allergies-lookups.component.ts
+++ EClinic-Client/src/app/modules/system-lookups/allergies-lookups/allergies-lookups.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { AccountAllergicsServiceProxy } from '../../../../shared/service-proxies/service-proxies';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
+import { AccountAllergicsServiceProxy, LookupDto } from '../../../../shared/service-proxies/service-proxies';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'allergies-lookups',
@@ -8,12 +12,32 @@ import { AccountAllergicsServiceProxy } from '../../../../shared/service-proxies
 })
 export class AllergiesLookupsComponent implements OnInit {
 
+  dataSource: MatTableDataSource<LookupDto>;
+  displayedColumns: string[] = ['ID', 'name', 'Status'];
+  data: LookupDto[];
+
+  @ViewChild('allergicsTable', {static: true}) table: MatTable<LookupDto>;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
   constructor(private _allergiesServiceProxy: AccountAllergicsServiceProxy) { }
 
   ngOnInit(): void {
-    this._allergiesServiceProxy.allergies2().subscribe(res =>{
-      console.log(res);
+    this._allergiesServiceProxy.allergies2().pipe(finalize(()=> {
+      this.setDataSource(this.data)
+    }))
+    .subscribe(res =>{
+      if(res.isSuccessful){
+        this.data = res.data.allergies;
+      }
     })
   }
+
+  setDataSource(data){
+    this.dataSource = new MatTableDataSource<LookupDto>(data);
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+   }
 
 }
--- EClinic-Client/src/app/modules/system-lookups/system-lookups.module.ts
+++ EClinic-Client/src/app/modules/system-lookups/system-lookups.module.ts
@@ -5,6 +5,43 @@ import { AllergiesLookupsComponent } from './allergies-lookups/allergies-lookups
 import { SystemLookupsComponent } from './system-lookups.component';
 import {NgbDropdownModule} from '@ng-bootstrap/ng-bootstrap';
 
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatSortModule} from '@angular/material/sort';
+import {MatTableModule} from '@angular/material/table';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatTreeModule} from '@angular/material/tree';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CoreModule } from 'src/app/_metronic/core';
+
 const routes: Routes = [
   {
     path: '',
@@ -22,6 +59,44 @@ const routes: Routes = [
     CommonModule,
     RouterModule.forChild(routes),
     NgbDropdownModule,
+    // material modules
+    MatInputModule,
+    MatBadgeModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatAutocompleteModule,
+    MatListModule,
+    MatSliderModule,
+    MatCardModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    MatNativeDateModule,
+    MatSlideToggleModule,
+    MatCheckboxModule,
+    MatMenuModule,
+    MatTabsModule,
+    MatTooltipModule,
+    MatSidenavModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatBottomSheetModule,
+    MatExpansionModule,
+    MatDividerModule,
+    MatSortModule,
+    MatStepperModule,
+    MatChipsModule,
+    MatPaginatorModule,
+    MatDialogModule,
+    MatRippleModule,
+    CoreModule,
+    MatRadioModule,
+    MatTreeModule,
+    MatButtonToggleModule,
   ]
 })
 export class SystemLookupsModule { }
